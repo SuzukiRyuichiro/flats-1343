d84baee8254f7b243bc516e1e7657616b8ee56be
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-14 18:14:05 +0100

    livecode done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,11 +2,24 @@ require 'open-uri'
 
 class FlatsController < ApplicationController
   def index
+    @flats = get_flats
+    # pass that array as an instance variable to the view (index.html.erb)
+  end
+
+  def show
+    # find the hash with the id in the url
+    flats = get_flats
+    # assing that single hash as an instance variable and pass it onto view
+    @flat = flats.find { |flat| flat['id'] == params[:id].to_i }
+  end
+
+  private
+
+  def get_flats # making the API request and returning an array of flat hashes
     url = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json'
     # make a request to the API
     json = URI.open(url).read
-    @flats = JSON.parse(json)
     # parse the json into an array of hashes
-    # pass that array as an instance variable to the view (index.html.erb)
+    JSON.parse(json)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'flats#index'
+  get '/flats/:id', to: 'flats#show', as: :flat
 end
